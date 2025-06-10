133a7225cf9b67754c9bd5e420b78521da544688
Sivaprasad Murali <sivaprasad.off@gmail.com>
2025-06-10 22:42:20 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .fylex import copy_files, move_files, JUNK_EXTENSIONS
-from .exceptions import *
-
-__version__ = "0.3.0"
-__all__ = ["copy_files", "move_files", "JUNK_EXTENSIONS", "FylexError", "InvalidPathError", "CopyFailedError"]
